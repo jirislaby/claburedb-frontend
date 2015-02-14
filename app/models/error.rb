deb8a32bd4e3337fff265d1a67de870244fc178e
Jiri Slaby <jirislaby@gmail.com>
2015-02-14 11:28:12 +0100

    models: error, set primary key
    I don't know why, but it doesn't work without that.

--- app/models/error.rb
+++ app/models/error.rb
@@ -1,5 +1,6 @@
 class Error < ActiveRecord::Base
   self.table_name = 'error'
+  self.primary_key = 'id'
   belongs_to :user, :foreign_key => "user"
   belongs_to :error_type, :foreign_key => "error_type"
   belongs_to :project_info, :foreign_key => "project"
