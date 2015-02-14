568b2fcf51d2bc0c9f2a4bd1da98b5e28576024a
Jiri Slaby <jirislaby@gmail.com>
2015-02-14 10:31:47 +0100

    claburedb.sh: pass proper bind ip

--- claburedb.sh
+++ claburedb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while :; do
-	bundle exec rails server -e production
+	bundle exec rails server -b 0.0.0.0 -e production
 	sleep 30
 	echo clabure restarted | mail -s 'clabure restarted' xslaby
 done
