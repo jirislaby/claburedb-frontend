32edebbe24f8f4cddda7a06da4c4359eee830ed7
Jiri Slaby <jirislaby@gmail.com>
2024-02-12 08:30:50 +0100

    cleanup
    Remove claburedb.sh (in favor of systemd unit file).

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,13 @@
+//= link errors.js
+//= link jquery
+//= link application
+//= link coderay
+
+//= link cwe
+//= link cwe10
+//= link favicon
+//= link info
+//= link info10
+//= link loader
+//= link rails
+
--- claburedb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bundle exec rails server -b 0.0.0.0 -e production
-sleep 30
-echo clabure restarted | mail -s 'clabure restarted' xslaby
-exec $0
